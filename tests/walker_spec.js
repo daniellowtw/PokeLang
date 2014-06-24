56a48a89df0b4c8614bbbadb0f995d107628fef6
Jit <jyy112@imperial.ac.uk>
2014-06-25 00:51:28 +0100

    Added semantic checking for known moves, turn order and move effectiveness

--- tests/walker_spec.js
+++ tests/walker_spec.js
@@ -11,6 +11,76 @@ describe('walker', function() {
   it('should walk an empty ast', function() {
 
     expect(W.walk([])).toEqual([]);
+  
+  });
+
+  it('should walk a simple ast', function() {
+
+    var ast = ["PROG", 
+               ["START", ["GO", "CHARMANDER"],
+                ["SENDS_OUT", "BUTTERFREE"]
+               ],
+               ["CONCAT", ["ENEMYTURN", "BUTTERFREE", "TACKLE"],
+                ["CONCAT", ["SELFTURN", "CHARMANDER", "SCRATCH"], null
+                ]
+               ]
+              ]
+
+    expect(W.walk(ast)).toEqual(['TACKLE', 'SCRATCH']);
+  
+  });
+
+  it('should throw an error when Pokemon does not know move', function() {
+
+    var ast = ["PROG", 
+               ["START", ["GO", "GEODUDE"],
+                ["SENDS_OUT", "GEODUDE"]
+               ],
+               ["CONCAT", ["ENEMYTURN", "GEODUDE", "SCRATCH"],
+                ["CONCAT", ["EFFECTIVE"],
+                 ["CONCAT", ["SELFTURN", "GEODUDE", "SCRATCH"], null
+                 ]
+                ]
+               ]
+              ]
+
+    expect(function() {W.walk(ast);}).toThrow('GEODUDE does not know move SCRATCH');
+  
+  });
+
+  it('should throw an error when turn order is wrong', function() {
+
+    var ast = ["PROG", 
+               ["START", ["GO", "PIKACHU"],
+                ["SENDS_OUT", "STARYU"]
+               ],
+               ["CONCAT", ["ENEMYTURN", "STARYU", "TACKLE"],
+                ["CONCAT", ["EFFECTIVE"],
+                 ["CONCAT", ["ENEMYTURN", "STARYU", "TACKLE"], null
+                 ]
+                ]
+               ]
+              ]
+
+    expect(function() {W.walk(ast);}).toThrow('Turn order is wrong: enemyTurnTaken');
+  
+  });
+  it('should throw an error on incorrect modifier', function() {
+
+    var ast = ["PROG", 
+               ["START", ["GO", "GEODUDE"],
+                ["SENDS_OUT", "GEODUDE"]
+               ],
+               ["CONCAT", ["ENEMYTURN", "GEODUDE", "TACKLE"],
+                ["CONCAT", ["EFFECTIVE"],
+                 ["CONCAT", ["SELFTURN", "GEODUDE", "SCRATCH"], null
+                 ]
+                ]
+               ]
+              ]
+
+    expect(function() {W.walk(ast);}).toThrow('TACKLE should be not very effective on GEODUDE');
+  
   });
 
 });
