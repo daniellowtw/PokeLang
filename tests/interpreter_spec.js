e21d1a20b8a7c82788e6cdddd284e49d206ec1cf
Jit <jyy112@imperial.ac.uk>
2014-06-24 15:51:27 +0100

    Added control flow and dictionary instructions. Recursion works.

--- tests/interpreter_spec.js
+++ tests/interpreter_spec.js
@@ -77,3 +77,134 @@ describe('exec', function() {
   });
 
 });
+
+describe('while', function() {
+
+  it('should perform a while loop', function() {
+  
+    var expr = I.block([I.operation('dup')
+                       ,I.num(2)
+                       ,I.operation('>')
+                       ]);
+
+    var block = I.block([I.operation('pop')
+                        ]);
+
+    var prog = [I.num(1)
+               ,I.num(2)
+               ,I.num(3)
+               ,I.num(4)
+               ,I.num(5)
+               ,expr
+               ,block
+               ,I.operation('while')
+               ];
+
+    var result = [I.num(1)
+                 ,I.num(2)
+                 ];
+
+    expect(I.run(prog)).toEqual(result);
+
+  });
+
+  it('should sum a list of numbers', function() {
+  
+    var expr = I.block([I.operation('dup')
+                       ,I.num(0)
+                       ,I.operation('>')
+                       ]);
+
+    var block = I.block([I.operation('+')
+                        ,I.operation('swap')
+                        ]);
+
+    var prog = [I.num(0)
+               ,I.num(1)
+               ,I.num(2)
+               ,I.num(3)
+               ,I.num(4)
+               ,I.num(5)
+               ,expr
+               ,block
+               ,I.operation('while')
+               ,I.operation('+')
+               ];
+
+    var result = [I.num(15)
+                 ];
+
+    expect(I.run(prog)).toEqual(result);
+
+  });
+
+});
+
+describe('dictionary', function() {
+
+  it('should store and load an object', function() {
+
+    var block = I.block([I.operation('+')]);
+
+    var prog = [I.num(1)
+               ,block
+               ,I.operation('store')
+               ,I.num(5)
+               ,I.num(10)
+               ,I.num(1)
+               ,I.operation('load')
+               ,I.operation('exec')
+               ];
+
+    var result = [I.num(15)];
+
+    expect(I.run(prog)).toEqual(result);
+
+  });
+
+});
+
+describe('power', function() {
+
+  it('should perform power through recursion', function() {
+
+    var thenblock = I.block([I.num(1)
+                            ,I.operation('-')
+                            ,I.operation('swap')
+                            ,I.operation('dup')
+                            ,I.operation('rot3')
+                            ,I.operation('rot3')
+                            ,I.num(1)
+                            ,I.operation('load')
+                            ,I.operation('exec')
+                            ,I.operation('*')
+                            ]);
+
+    var elseblock = I.block([I.operation('pop')
+                            ]);
+
+    var block = I.block([I.operation('dup')
+                        ,I.num(1)
+                        ,I.operation('>')
+                        ,thenblock
+                        ,elseblock
+                        ,I.operation('ifelse')
+                        ]);
+
+    var prog = [I.num(1)
+               ,block
+               ,I.operation('store')
+               ,I.num(5)
+               ,I.num(3)
+               ,I.num(1)
+               ,I.operation('load')
+               ,I.operation('exec')
+               ];
+
+    var result = [I.num(125)];
+
+    expect(I.run(prog)).toEqual(result);
+
+  });
+
+});
