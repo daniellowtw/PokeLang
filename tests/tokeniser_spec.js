18ffd0071999286b184c18fe8e3e98562651fcaa
Jit <jyy112@imperial.ac.uk>
2014-06-24 17:48:52 +0100

    Created unit tests for tokeniser

---
+++ tests/tokeniser_spec.js
@@ -0,0 +1,86 @@
+// require jasmine-node
+var I = require('../lib/interpreter/interpreter.js');
+var T = require('../lib/interpreter/tokeniser.js');
+
+describe('tokeniser', function() {
+
+  it('should tokenise empty strings', function() {
+
+    var string = '';
+
+    var result = [
+                 ];
+
+    expect(T.tokenise(string)).toEqual(result);
+
+  });
+
+  it('should tokenise numbers and operations correctly', function() {
+
+    var string = '1 2 3';
+
+    var result = [I.num(1)
+                 ,I.num(2)
+                 ,I.num(3)
+                 ];
+
+    expect(T.tokenise(string)).toEqual(result);
+
+    string = '1 2 3 +';
+
+    result = [I.num(1)
+             ,I.num(2)
+             ,I.num(3)
+             ,I.operation('+')
+             ];
+
+    expect(T.tokenise(string)).toEqual(result);
+
+  });
+
+  it('should tokenise blocks correctly', function() {
+
+    var string = '1 2 3 { + - } load';
+
+    var result = [I.num(1)
+                 ,I.num(2)
+                 ,I.num(3)
+                 ,I.block([I.operation('+')
+                          ,I.operation('-')
+                          ])
+                 ,I.operation('load')
+                 ];
+
+    expect(T.tokenise(string)).toEqual(result);
+
+  });
+
+  it('should tokenise nested blocks correctly', function() {
+
+    var string = '1 2 3 { + { swap } +  } load';
+
+    var result = [I.num(1)
+                 ,I.num(2)
+                 ,I.num(3)
+                 ,I.block([I.operation('+')
+                          ,I.block([I.operation('swap')])
+                          ,I.operation('+')
+                          ])
+                 ,I.operation('load')
+                 ];
+
+    expect(T.tokenise(string)).toEqual(result);
+
+  });
+
+  it('should throw error if a block is not closed', function() {
+
+    var string = '1 2 3 { + { swap } +  load';
+
+    var result = 'Block not closed.';
+
+    expect(function(){T.tokenise(string);}).toThrow(result);
+
+  });
+
+});
