accc1e2483dd2a815b20edd53c0f77b4cf072fb4
Jit <jyy112@imperial.ac.uk>
2014-06-24 17:50:13 +0100

    Merged with tokeniser

--- tests/interpreter_spec.js
+++ tests/interpreter_spec.js
@@ -1,7 +1,9 @@
 // require jasmine-node
 var I = require('../lib/interpreter/interpreter.js');
+var T = require('../lib/interpreter/tokeniser.js');
 
 I.init();
+T.init();
 
 describe('addition', function() {
 
@@ -11,6 +13,18 @@ describe('addition', function() {
                ,I.num(2)
                ,I.operation('+')
                ];
+
+    var result = [I.num(3)];
+
+    expect(I.run(prog)).toEqual(result);
+
+  });
+
+  it('should add 1 and 2 via tokeniser', function() {
+
+    var string = '1 2 +';
+     
+    var prog = T.tokenise(string);
 
     var result = [I.num(3)];
 
@@ -191,20 +205,72 @@ describe('power', function() {
                         ,I.operation('ifelse')
                         ]);
 
-    var prog = [I.num(1)
-               ,block
-               ,I.operation('store')
-               ,I.num(5)
-               ,I.num(3)
-               ,I.num(1)
-               ,I.operation('load')
-               ,I.operation('exec')
-               ];
+    var powerProg = function(x, y) {
+      
+      return [I.num(1)
+	     ,block
+             ,I.operation('store')
+             ,I.num(x)
+             ,I.num(y)
+             ,I.num(1)
+             ,I.operation('load')
+             ,I.operation('exec')
+             ];
 
-    var result = [I.num(125)];
+    }
 
-    expect(I.run(prog)).toEqual(result);
+    expect(I.run(powerProg(5,3))).toEqual([I.num(125)]);
+
+    expect(I.run(powerProg(1,5))).toEqual([I.num(1)]);
+
+    expect(I.run(powerProg(2,1))).toEqual([I.num(2)]);
+
+  });
+
+  it('should perform power through recursion via tokeniser', function() {
+
+    var thenblock = I.block([I.num(1)
+                            ,I.operation('-')
+                            ,I.operation('swap')
+                            ,I.operation('dup')
+                            ,I.operation('rot3')
+                            ,I.operation('rot3')
+                            ,I.num(1)
+                            ,I.operation('load')
+                            ,I.operation('exec')
+                            ,I.operation('*')
+                            ]);
+
+    var elseblock = I.block([I.operation('pop')
+                            ]);
+
+    var block = I.block([I.operation('dup')
+                        ,I.num(1)
+                        ,I.operation('>')
+                        ,thenblock
+                        ,elseblock
+                        ,I.operation('ifelse')
+                        ]);
+
+    var power = function(x, y) {
+
+      var string = '1 { dup 1 > ' +
+                     '{ 1 - swap dup rot3 rot3 1 load exec * } ' +
+                     '{ pop } ' +
+                     'ifelse '+
+                   '} store ' + x + ' ' + y + ' 1 load exec';
+
+      return string;
+
+    }
+
+    expect(I.run(T.tokenise(power(5,3)))).toEqual([I.num(125)]);
+
+    expect(I.run(T.tokenise(power(1,3)))).toEqual([I.num(1)]);
+
+    expect(I.run(T.tokenise(power(2,1)))).toEqual([I.num(2)]);
 
   });
 
 });
+
