e3dfa38b3a7df08f8349743d755417246890dc3c
Jit <jyy112@imperial.ac.uk>
2014-06-25 18:40:51 +0100

    Created tests for parser, walker and interpreter

---
+++ tests/parser_walker_interpreter.js
@@ -0,0 +1,12 @@
+var P = require('../lib/parser/pokemon.js'),
+    W = require('../lib/walker/walker.js'),
+    T = require('../lib/interpreter/tokeniser.js'),
+    I = require('../lib/interpreter/interpreter.js');
+
+describe('parser, walker and interpreter', function() {
+
+  it('should import with no error', function() {
+  
+  });
+
+});
